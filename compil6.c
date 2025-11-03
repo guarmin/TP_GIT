f3aac1361a3bc45c4af485ca04ecf281b8bc59a3
Garmin <louis.guerin17220@gmail.com>
2025-11-03 14:16:14 +0100

    correction compil6

---
+++ compil6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    if (x == 5) {
+        printf("x vaut 5\n");
+    }
+    return 0;
+}
+/* l'erreur etait l'utilisation de l'opérateur d'affectation '=' au lieu de l'opérateur de comparaison '==' ligne 4 */
