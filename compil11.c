38c6fa1572f080d68f785799925b67532d599207
Garmin <louis.guerin17220@gmail.com>
2025-11-03 14:41:53 +0100

    correcction compil11

---
+++ compil11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    if (1) {
+        printf("ok\n");
+    } else {
+        printf("nope\n");
+        }
+    return 0;
+}
+/* l'erreur etait l'absence d'accolade qui ouvre le else ligne 6 */
