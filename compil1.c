73e09edd640a1584b96be1868f0c1aa81e39d403
Garmin <louis.guerin17220@gmail.com>
2025-11-03 13:46:36 +0100

    correctif point virgule

---
+++ compil1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello world\n");
+    return 0;
+}
+
+/*compil1.c: In function 'main':
+compil1.c:5:5: error: expected ';' before 'return'
+     return 0;
+
+l'erreur est due à l'absence d'un point virgule à la fin de la ligne 4*/
