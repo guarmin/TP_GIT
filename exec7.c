c241c0570106687c95be0e5e7a9460f1b61f59d0
Garmin <louis.guerin17220@gmail.com>
2025-11-03 15:35:13 +0100

    correction exec7

---
+++ exec7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int distance_km = 100;
+    int vitesse_kmh = 50;
+    int temps =(float) distance_km / vitesse_kmh; 
+    int minutes =(int) temps * 60;
+    printf("minutes = %d\n", minutes); 
+    return 0;
+}
+/* le probleme etait que le temps etait en int donc la division donnait un resultat entier il fallait le passer en float pour avoir un resultat decimal ensuite pour les minutes il fallait le repasser en int */
