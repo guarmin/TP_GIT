2107ce3e6c8571ff42ddd29b0ea7c185b4a092ed
Garmin <louis.guerin17220@gmail.com>
2025-11-20 21:34:29 +0100

    c'est pas beau mais au moins ca marche : )

--- tp 3/tp3.c
+++ tp 3/tp3.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-    int n = 0;
+    int n;
     printf("Saisissez une taille pour l'etoile ( entre 5 et 10 ) : ");
     scanf("%d", &n);
 
     int width = n * n;
-    int max_height = 3 * n - 1;
+    int max_height = n * 3 - 1;
 
-    char img[100][100];
+    char img[50][150];
     int mid = width / 2;
     for (int i = 0; i < max_height; i++)
     {
@@ -18,39 +18,82 @@ int main()
             img[i][j] = ' ';
         }
     }
+
+    /* la pointe */
     img[0][mid] = 'A';
+
     for (int i = 1; i < n; i++)
     {
-        for (int j = 0; j < width; j++)
+        if (i == n - 1)
         {
-            if (img[i - 1][j] != ' ')
-            {
-                img[i][j] = 's';
-            }
-            else if (j == mid - i || j == mid + i)
-            {
-                img[i][j] = 'A';
-            }
-            else if (i == n - 1)
+            for (int j = 0; j < width; j++)
             {
                 img[i][j] = '_';
             }
         }
+        for (int j = 0; j < i * 2 + 1; j++)
+        {
+            if (j == 0 || j == i * 2)
+            {
+                img[i][mid + (j - i)] = 'A';
+            }
+            else
+            {
+                img[i][mid + (j - i)] = 'S';
+            }
+        }
     }
-    for (int i = n; i < n * 2 - n / 5; i++)
+
+    /* le corps */
+
+    for (int i = n; i < n * 2 - 2; i++)
+    {
+        int b = 2 * i - 2 * n;
+        for (int j = b; j < width - b; j++)
+        {
+            img[i][j] = 'S';
+
+            if (j == b || j == width - b - 1)
+            {
+                img[i][j] = '"';
+                img[i][b + 1] = 'V';
+                img[i][width - b - 2] = 'V';
+            }
+        }
+    }
+
+    /* le bas */
+
+    int x = n * 2 - 2;
+    for (int i = 0; i < n + 1; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            if (i == n && j == 0 || j == width - 1 && i == n)
+            img[x][j] = 'S';
+        }
+        for (int j = 0; j < 2 * n - 4 - i; j++)
+        {
+            img[x][j] = ' ';
+            img[x][width - j - 1] = ' ';
+            if (j == 2 * n - 5 - 1)
             {
-                img[i][j] = '"';
-            }
-
-            else if (i == n)
-            {
-                img[i][j] = 'S';
+                img[x][width - j - 1] = '.';
+                img[x][j] = '.';
             }
         }
+        for (int j = mid - 2 * i; j < mid; j++)
+        {
+            img[x][j] = ' ';
+            img[x][width - j - 1] = ' ';
+            img[x][mid] = ' ';
+            if (j == mid - 2 * i)
+            {
+                img[x][width - j - 1] = '\"';
+                img[x][j] = '\"';
+            }
+        }
+
+        x++;
     }
 
     for (int i = 0; i < max_height; i++)
