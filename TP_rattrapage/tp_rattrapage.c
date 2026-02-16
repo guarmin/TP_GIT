40df2194366123469436ff4a6f59f024c4b1a158
Garmin <louis.guerin17220@gmail.com>
2026-02-16 15:02:38 +0100

    amelioration/correction fonction inscrption

--- TP_rattrapage/tp_rattrapage.c
+++ TP_rattrapage/tp_rattrapage.c
@@ -13,21 +13,26 @@ int joueur1 = 0;
 int joueur2 = 0;
 int win = 0;
 
-char inscription(nomJoueur, ageJoueur, scoreJoueur, numeroMatch, joueur1, joueur2, win)
+char inscription(nomJoueur, ageJoueur, scoreJoueur, numeroMatch, joueur1, joueur2, win) /* fonction qui prend en compte les differentes donnees*/
 {
 
-    for (int i = 0; i <= 7; i++)
+    for (int i = 0; i <= 7; i++) /* inscrption du nom du joueur*/
     {
         char tableauJoueur[i][nomJoueur];
-        for (int j = 1; i <= 8; i++)
+        for (int j = 1; j <= 8; j++)
         {
             printf(" Veuillez entrez le nom du joueur %d: ", j);
             scanf("%c", &nomJoueur);
         }
     }
-    for (int j = 0; j <= 7; j++)
+    for (int i = 0; i <= 7; i++) /* inscription de l'age du joueur*/
     {
-        int tableauAge[j][ageJoueur];
+        int tableauAge[i][ageJoueur];
+        for (int j = 1; j <= 8; j++)
+        {
+            printf(" Veuillez entrez l'age du joueur %d: ", j);
+            scanf("%c", &ageJoueur);
+        }
     }
 }
 int main()
