a80ce39a367a962b90678f472dc324d4f9060ae4
Garmin <louis.guerin17220@gmail.com>
2026-02-16 14:40:15 +0100

    etape 1 + debut etape 2

---
+++ TP_rattrapage/tp_rattrapage.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* donné neccessaire des joueurs */
+char nomJoueur[15];
+int ageJoueur[2];
+int scoreJoueur[2];
+
+/* donnée neccesaire pour creer les matchs */
+
+int numeroMatch = 0;
+int joueur1 = 0;
+int joueur2 = 0;
+int win = 0;
+
+char inscription(nomJoueur, ageJoueur, scoreJoueur, numeroMatch, joueur1, joueur2, win)
+{
+    for (int i = 0; i >= 7; i++)
+    {
+        char tableauJoueur[nomJoueur];
+    }
+}
+int main()
+{
+}
